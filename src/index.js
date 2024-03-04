eaf39eaba24bc636d168d3d9eecee2d134ad2766
il4mb <durianbohong@gmail.com>
2024-03-05 00:57:22 +0700

    Changes to be committed: 	modified:   dist/main.css 	modified:   dist/main.js 	modified:   index.html 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app.scss 	modified:   src/index.js 	modified:   yarn.lock

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "./app.scss";
 import 'animate.css';
 import $ from "jquery";
 import '../assets/fontawesome/css/all.min.css';
+import "../assets/bootstrap/dist/js/bootstrap.min.js";
 
 window.$ = $;
 
